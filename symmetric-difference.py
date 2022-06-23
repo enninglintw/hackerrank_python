e53f2a6c1c36561bde0a3113fb63ec8f84383958
En-ning Lin <enninglintw@gmail.com>
2022-06-23 19:25:14 +0800

    Refactor symmetric-difference

--- symmetric-difference.py
+++ symmetric-difference.py
@@ -3,10 +3,7 @@ m = set(map(int, input().split()))
 n_length = input()
 n = set(map(int, input().split()))
 
-union = m.union(n)
-intersection = m.intersection(n)
-symmetric_difference = union.difference(intersection)
-symmetric_difference_ints = sorted(list(symmetric_difference))
+symmetric_difference_ints = sorted(list(m ^ n))
 
 for int in symmetric_difference_ints:
     print(int)
